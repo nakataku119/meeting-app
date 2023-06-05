9db256fd99d3ae7c8b0ab78ae2171b0ec05b223b
takuya nakahara <takuya.nakahara@genit.jp>
2023-06-05 18:56:08 +0900

    コメントの追加

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Add your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
